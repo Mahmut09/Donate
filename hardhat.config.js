8c17341641ddeb3b5845a76a89c8a9e139340562
Махмут <48020556+Mahmut09@users.noreply.github.com>
2022-01-10 18:27:29 +0600

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,7 @@
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
-const rinkeby_PRIVATE_KEY = "";  //7e923d52886172411a38d2a440a4d2fadf7e6a2819294d4cb7c75279363f65a2
+const rinkeby_PRIVATE_KEY = "bc390e19915b77368091cf54e02d36d3e93142443a24769f48a698f63ba6ce0b";
 
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-web3");
@@ -12,7 +12,7 @@ module.exports = {
 
   networks: {
     rinkeby: {
-      url: "https://rinkeby.infura.io/v3/207f9313845e42f2ab2469a252ba1d10",
+      url: "https://mainnet.infura.io/v3/54a940df53984540bcacdc04144a630c",
       accounts: [ `${rinkeby_PRIVATE_KEY}` ],
     },
   },
